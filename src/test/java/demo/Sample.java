6a7c766829bcd10226917fb2ce6f4195d8bab30b
Rajath <Rajath@LAPTOP-D66QITOR>
2023-10-01 14:15:37 +0530

    Commit and Push

---
+++ src/test/java/demo/Sample.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("Hi, this is my first commit");
+	}
+}
+//This class is being created to understand the working of GitHub.
+//In order to create a local repository, right-click on project, click on team and select share project. 
